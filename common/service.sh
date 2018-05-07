a0d553d3b38847a1d48f637213a91ddb01dc07c6
Zackptg5 <zackptg5@comcast.net>
2018-05-07 19:45:57 -0400

    Add mediaserver kil

--- common/service.sh
+++ common/service.sh
@@ -16,3 +16,4 @@ fi
 am start -a android.intent.action.MAIN -n com.vipercn.viper4android.xhifi/.main.ViPER4Android_XHiFi
 killall com.vipercn.viper4android.xhifi
 killall audioserver
+killall mediaserver
